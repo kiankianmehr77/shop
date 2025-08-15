ecd1c07e1979e6ef9cfa3a7d5d5f644f09d2292c
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-08-15 20:44:22 +0330

    first step for otp code

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
-from .models import User
+from .models import User, OtpCode
 from .forms import UserCreationForm, UserChangeForm
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'code', 'created')
 
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
